ce7cf5f5b0b3ed69e9af4e006ec6d74157c13ce6
ShadowHD33RUS <shadowhd33rus@gmail.com>
2021-12-11 02:21:05 +0300

    Add amogus command

---
+++ commands/amogus.js
@@ -0,0 +1,12 @@
+const i18n = require("../util/i18n");
+const { execute: play } = require("./play");
+
+module.exports = {
+  name: "amogus",
+  cooldown: 3,
+  description: i18n.__("amogus.description"),
+  async execute(message) {
+    const track = Math.floor(Math.random() * max) ? 'https://youtu.be/jKLG0vLhWFI' : 'https://youtu.be/rcWcrL2o9bQ'
+    await play(message, [track, '!!']);
+  }
+};
