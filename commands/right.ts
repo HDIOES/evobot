8dea1325cdf7a8d76f2dc0d34f8d742df138d026
ShadowHDone <ShadowHDone@gmail.com>
2023-03-06 01:00:27 +0300

    Update for new reality

--- commands/right.ts
+++ commands/right.ts
@@ -1,12 +1,23 @@
+import { ChatInputCommandInteraction, PermissionsBitField, SlashCommandBuilder, TextChannel } from "discord.js";
 import { Message } from "discord.js";
 import { i18n } from "../utils/i18n";
 import play from "./play";
 
 export default {
+  data: new SlashCommandBuilder()
+    .setName("right")
+    .setDescription(i18n.__("right.description"))
+    .addStringOption((option) => option.setName("song").setDescription("The song you want to play").setRequired(true)),
+
   name: "right",
   cooldown: 3,
-  description: i18n.__("right.description"),
-  async execute(message: Message, args: string[]) {
-    await play.execute(message, ["right", "version", ...args]);
+  permissions: [
+    PermissionsBitField.Flags.Connect,
+    PermissionsBitField.Flags.Speak,
+    PermissionsBitField.Flags.AddReactions,
+    PermissionsBitField.Flags.ManageMessages
+  ],
+  async execute(interaction: ChatInputCommandInteraction, input: string) {
+    await play.execute(interaction, `${input} right version`);
   }
 };
